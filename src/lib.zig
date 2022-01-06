d4d85e2542314211c39c7bb924bd92a16c69d4fd
Yurii Rashkovskii <yrashk@gmail.com>
2022-01-06 09:41:44 -0800

    Problem: limited extensibility
    Composition of contracts is great, but what if we want to establish
    further conceptual contracts over data types?
    
    Solution: experiment with `contracts` declarations
    that would allow types to supply information to actual contracts.
    
    First example is the establishment of equivalency -- types that are not
    equal but "equivalent" (whatever that means for the domain logic).
    
    This example necessitates addition of `contracts.isEquivalent(type) -> bool`
    function to at least one of the types.
    
    I ran into the case of not being able to compare generic function types, so
    I hand-rolled one, but it is still imperfect as it can't get type information
    of the return (or so it seems).

--- src/lib.zig
+++ src/lib.zig
@@ -300,9 +300,34 @@ test "Outcome.named" {
     }
 }
 
+fn fnArgsEql(comptime a: []const std.builtin.TypeInfo.FnArg, comptime b: []const std.builtin.TypeInfo.FnArg) bool {
+    if (a.len != b.len) return false;
+    if (a.ptr == b.ptr) return true;
+    for (a) |item, index| {
+        if (b[index].is_generic != item.is_generic) return false;
+        if (b[index].is_noalias != item.is_noalias) return false;
+        if (b[index].arg_type == null and item.arg_type != null) return false;
+        if (b[index].arg_type != null and item.arg_type != null and b[index].arg_type.? != item.arg_type.?) return false;
+    }
+    return true;
+}
+
+fn isGenericFnEqual(comptime T: type, comptime T1: type) bool {
+    comptime {
+        if (@typeInfo(T) != .Fn or @typeInfo(T) != .Fn)
+            return false;
+        const ti = @typeInfo(T).Fn;
+        const ti1 = @typeInfo(T1).Fn;
+        return ti.calling_convention == ti1.calling_convention and ti.alignment == ti1.alignment and
+            ((ti.return_type == null and ti1.return_type == null) or (ti.return_type.? == ti1.return_type.?)) and
+            fnArgsEql(ti.args, ti1.args);
+    }
+}
+
 /// A contract that requires type `T` to be the same type as `T1`
 pub fn is(comptime T: type, comptime T1: type) Outcome {
-    return Outcome.init(T == T1, Invalid{
+    const valid = if (isGenericFn(T) == .Valid and isGenericFn(T1) == .Valid) isGenericFnEqual(T, T1) else T == T1;
+    return Outcome.init(valid, Invalid{
         .identifier = std.fmt.comptimePrint("is({}, {})", .{ T, T1 }),
     });
 }
@@ -313,6 +338,30 @@ test "is" {
         try expect(is(u8, u16) == .Invalid);
         try expect(std.mem.eql(u8, "is(u8, u16)", is(u8, u16).identifier()));
         try expect(is(u8, u16).Invalid.causes.len == 0);
+        try expect(is(fn (u8) u8, fn (u8) u8) == .Valid);
+        try expect(is(fn (type) u8, fn (type) u8) == .Valid);
+        // FIXME: figure out what to do with the return type, it's not really validating it
+        // try expect(is(fn (type) u8, fn (type) bool) == .Invalid);
+    }
+}
+
+/// A contract that requires function type T be generic
+pub fn isGenericFn(comptime T: type) Outcome {
+    comptime {
+        const identifier = std.fmt.comptimePrint("isGenericFn({})", .{T});
+        return isType(T, .Fn).andThen(struct {
+            pub fn then() Outcome {
+                return Outcome.init(@typeInfo(T).Fn.is_generic, Invalid{ .identifier = identifier });
+            }
+        }).named(identifier);
+    }
+}
+
+test "isGenericFn" {
+    comptime {
+        try expect(isGenericFn(fn (type) bool) == .Valid);
+        try expect(isGenericFn(fn (bool) bool) == .Invalid);
+        try expect(isGenericFn(u8) == .Invalid);
     }
 }
 
@@ -320,7 +369,7 @@ test "is" {
 /// type (as in .Struct, .Int, etc.)
 ///
 /// Includes violation reason into `Invalid.reason`
-pub fn isType(comptime T: type, type_id: std.builtin.TypeId) Outcome {
+pub fn isType(comptime T: type, comptime type_id: std.builtin.TypeId) Outcome {
     return Outcome.init(@typeInfo(T) == type_id, Invalid{
         .identifier = std.fmt.comptimePrint("isType({}, .{s})", .{ T, @tagName(type_id) }),
         .reason = std.fmt.comptimePrint("got .{s}", .{@tagName(@typeInfo(T))}),
@@ -374,6 +423,162 @@ test "hasDecl" {
             a: void,
             const b = 1;
         }, "b") == .Valid);
+    }
+}
+
+/// A contract that requires that a given type is a struct,
+/// enum, union or an opaque type that has a struct declaration by the given name.
+pub fn hasStruct(comptime T: type, comptime name: []const u8) Outcome {
+    return hasDecl(T, name).andThen(struct {
+        pub fn then() Outcome {
+            return isType(@field(T, name), .Struct);
+        }
+    })
+        .named(std.fmt.comptimePrint("hasStruct({}, {s})", .{ T, name }));
+}
+
+test "hasStruct" {
+    comptime {
+        try expect(hasStruct(u8, "a") == .Invalid);
+        try expect(std.mem.eql(
+            u8,
+            hasStruct(u8, "a").Invalid.cause().Invalid.reason,
+            "u8 is not a struct, enum, union or an opaque type",
+        ));
+        try expect(hasStruct(struct {}, "a") == .Invalid);
+        try expect(hasStruct(struct {
+            const a = struct {};
+        }, "a") == .Valid);
+        try expect(hasStruct(enum {
+            a,
+        }, "a") == .Invalid);
+        try expect(hasStruct(enum {
+            a,
+            const b = struct {};
+        }, "b") == .Valid);
+
+        try expect(hasStruct(union(enum) {
+            a: void,
+        }, "a") == .Invalid);
+        try expect(hasStruct(union(enum) {
+            a: void,
+            const b = struct {};
+        }, "b") == .Valid);
+    }
+}
+
+/// A contract that requires that a given type is a struct,
+/// enum, union or an opaque type that has a function declaration by the given name.
+pub fn hasFn(comptime T: type, comptime name: []const u8) Outcome {
+    return hasDecl(T, name).andThen(struct {
+        pub fn then() Outcome {
+            return isType(@TypeOf(@field(T, name)), .Fn);
+        }
+    })
+        .named(std.fmt.comptimePrint("hasFn({}, {s})", .{ T, name }));
+}
+
+test "hasFn" {
+    comptime {
+        try expect(hasFn(u8, "a") == .Invalid);
+        try expect(std.mem.eql(
+            u8,
+            hasFn(u8, "a").Invalid.cause().Invalid.reason,
+            "u8 is not a struct, enum, union or an opaque type",
+        ));
+        try expect(hasFn(struct {}, "a") == .Invalid);
+        try expect(hasFn(struct {
+            fn a() void {}
+        }, "a") == .Valid);
+        try expect(hasFn(enum {
+            a,
+        }, "a") == .Invalid);
+        try expect(hasFn(enum {
+            a,
+            fn b() void {}
+        }, "b") == .Valid);
+
+        try expect(hasFn(union(enum) {
+            a: void,
+        }, "a") == .Invalid);
+        try expect(hasFn(union(enum) {
+            a: void,
+            fn b() void {}
+        }, "b") == .Valid);
+    }
+}
+
+fn isEquivalent_(comptime A: type, comptime B: type) Outcome {
+    return hasStruct(A, "contracts")
+        .andThen(struct {
+        pub fn then() Outcome {
+            comptime {
+                return hasFn(@field(A, "contracts"), "isEquivalent");
+            }
+        }
+    })
+        .andThen(struct {
+        pub fn then() Outcome {
+            return is(@TypeOf(A.contracts.isEquivalent), fn (type) bool);
+        }
+    })
+        .andThen(struct {
+        pub fn then() Outcome {
+            return Outcome.init(A.contracts.isEquivalent(B), Invalid{
+                .identifier = std.fmt.comptimePrint("{s}.contracts.isEquivalent({s})", .{ A, B }),
+            });
+        }
+    });
+}
+
+/// A contract that requires that a given types A and B are a struct, enum, union or
+/// an opaque type, and either A, or B, or both define `contracts` struct with `isEquivalent(type) bool`
+/// function, and that function of either A or B returns true.
+///
+/// This is used to establish equivalency of otherwise unequal types:
+///
+/// ```
+/// const A = struct {
+///   pub const contracts = struct {
+///     pub fn isEquivalent(comptime T: type) bool {
+///       return T == B;
+///     }
+///   };
+///   
+///   pub const contracts = struct {
+///     pub fn isEquivalent(comptime T: type) bool {
+///       return T == B;
+///     }
+///   };
+/// };
+///
+/// assert(isEquivalent(A, B) == .Valid);
+/// ```
+pub fn isEquivalent(comptime A: type, comptime B: type) Outcome {
+    return isEquivalent_(A, B).orElse(isEquivalent_(B, A));
+}
+
+test "isEquivalent" {
+    comptime {
+        const Tequiv = struct {
+            pub const contracts = struct {
+                pub fn isEquivalent(comptime _: type) bool {
+                    return true;
+                }
+            };
+        };
+        const Tnonequiv = struct {
+            pub const contracts = struct {
+                pub fn isEquivalent(comptime _: type) bool {
+                    return false;
+                }
+            };
+        };
+        _ = Tequiv;
+        _ = Tnonequiv;
+        try expect(isEquivalent(Tequiv, Tnonequiv) == .Valid);
+        try expect(isEquivalent(Tnonequiv, Tequiv) == .Valid);
+        try expect(isEquivalent(Tnonequiv, Tnonequiv) == .Invalid);
     }
 }
 
